8bf778c87eee20f0fb14945606d5be382509ad19
Avani Kale <avani.kale@thoughtworks.com>
2019-12-09 15:41:38 +0530

    [avani]Add. can print five to nine number in third row.

--- src/test/java/com/tw/numbersequence/NumberSequencePyramidPatternTest.java
+++ src/test/java/com/tw/numbersequence/NumberSequencePyramidPatternTest.java
@@ -13,10 +13,17 @@ public class NumberSequencePyramidPatternTest {
     }
 
     @Test
-    public void givenTwoNthAndThreeMthNumber_WhenPrint_ThenShouldPrintThreeSpaceAndThreeStar() {
+    public void givenTwoNthAndThreeMthNumber_WhenPrint_ThenShouldPrintOneToSixNumber() {
         NumberSequencePyramidPattern numberSequenceSquarePattern = new NumberSequencePyramidPattern(2, 3);
 
         Assertions.assertEquals(" 1 \n234", numberSequenceSquarePattern.print());
+
+    }
+    @Test
+    public void givenTwoNthAndThreeMthNumber_WhenPrint_ThenShouldPrintOneToNineNumber() {
+        NumberSequencePyramidPattern numberSequenceSquarePattern = new NumberSequencePyramidPattern(3, 5);
+
+        Assertions.assertEquals("  1  \n 234 \n56789", numberSequenceSquarePattern.print());
 
     }
 
